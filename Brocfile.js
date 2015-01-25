7f8ac32eab46f5a6f71554cb42ebf2d88c9fd154
Sachin <millisami@gmail.com>
2015-01-25 13:23:55 +0545

    embercli ratchet installed

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,12 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  'ember-cli-ratchet': {
+    'theme': 'ios',
+    'importRatchetJs': true
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
